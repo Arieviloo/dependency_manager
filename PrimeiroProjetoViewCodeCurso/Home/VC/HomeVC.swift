f955ac95878b5ce1a0137be9ba610333ac7da81e
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-07-10 14:39:58 -0300

    init

---
+++ PrimeiroProjetoViewCodeCurso/Home/VC/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  PrimeiroProjetoViewCodeCurso
+//
+//  Created by Caio Fabrini on 14/09/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
